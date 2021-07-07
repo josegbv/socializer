731bf8152998dc3ae504911b34570870b609d6fb
Jose Brito <ing.josebritovillarroel@gmail.com>
2021-07-06 21:23:00 -0400

    2do estado con formulario, 1pr part6

--- src/Vistas/SignUp.js
+++ src/Vistas/SignUp.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Main from '../Componentes/Main';
 import imagenSignup from '../Imagenes/signup.png';
 
-export default function Signup(){
 
-    const usuario = {
-        nombre:'',
-        email:'',
-        username:'',
-        bio:'',
-        password:''
-    }
+export default function Signup(){
+ const [usuario, setUsuario] = useState({
+    nombre:'juaco',
+    email:'',
+    username:'',
+    bio:'',
+    password:''
+ })
+    
 
     function Inputchange(e){
-        usuario[e.target.name] = [e.target.value];
+        setUsuario({
+            ...usuario,
+            [e.target.name] : [e.target.value]
+        })
+        
         console.log(usuario);
     }
     return (
@@ -25,12 +30,12 @@ export default function Signup(){
                     <p className="FormContainer__info">
                     Registrate para que veas el clon de Instagram</p>
                     <form>
-                        <input type="email" name="email" placeholder="Email" className="Form__field" onChange={Inputchange} required></input>
-                        <input type="text" name="nombre" placeholder="Nombre Completo" minLength="3" maxLength="50" className="Form__field" onChange={Inputchange} required></input>
-                        <input type="text" name="username" placeholder="Usuario"  minLength="3" maxLength="30" className="Form__field" onChange={Inputchange} required></input>
-                        <input type="text" name="bio" placeholder="Cuentanos de Ti!!"  maxLength="150"className="Form__field" onChange={Inputchange} required></input>
-                        <input type="password" name="password" placeholder="Contraseña" className="Form__field" onChange={Inputchange} required></input>  
-                        <button className="Form__submit" type="submit">Registrate</button>
+                        <input value={usuario.email} type="email" name="email" placeholder="Email" className="Form__field" onChange={Inputchange} required></input>
+                        <input value={usuario.nombre} type="text" name="nombre" placeholder="Nombre Completo" minLength="3" maxLength="50" className="Form__field" onChange={Inputchange} required></input>
+                        <input value={usuario.username} type="text" name="username" placeholder="Usuario"  minLength="3" maxLength="30" className="Form__field" onChange={Inputchange} required></input>
+                        <input value={usuario.bio} type="text" name="bio" placeholder="Cuentanos de Ti!!"  maxLength="150"className="Form__field" onChange={Inputchange} required></input>
+                        <input value={usuario.password} type="password" name="password" placeholder="Contraseña" className="Form__field" onChange={Inputchange} required></input>  
+                        <button  className="Form__submit" type="submit">Registrate</button>
                         <p className="FormContainer__info">Ya tienes cuenta? <a href="/login">Login</a></p>
                     </form>
                 </div>
